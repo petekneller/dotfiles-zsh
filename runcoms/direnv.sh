401d67cb613955aa63a64916efb375f729ecdcd6
Pete Kneller <pommypete@gmail.com>
2026-02-11 16:14:07 +0000

    Don't export direnv env vars if its not installed

--- runcoms/direnv.sh
+++ runcoms/direnv.sh
@@ -1,2 +1,5 @@
-export DIRENV_LOG_FORMAT='' # quieten direnv so p10k instant prompt doesn't complain about stdout during init
-[[ -f $(which direnv) ]] && eval "$(direnv hook zsh)"
+if [[ -f $(which direnv) ]]
+then
+  export DIRENV_LOG_FORMAT='' # quieten direnv so p10k instant prompt doesn't complain about stdout during init
+  eval "$(direnv hook zsh)"
+fi
