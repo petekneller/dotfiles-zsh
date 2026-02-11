714c95ac33599ccb277cc4b5928f8d98acbf3004
Pete Kneller <pommypete@gmail.com>
2026-02-11 16:31:54 +0000

    Adds pyenv

--- runcoms/python.sh
+++ runcoms/python.sh
@@ -1,4 +1,13 @@
 # pip user site
-if [[ "$OSTYPE" == darwin* ]]; then
+if [[ "$OSTYPE" == darwin* ]]
+then
   export PATH="${PATH}:/Users/peter.kneller/Library/Python/3.12/bin"
+fi
+
+# pyenv
+if [[ -f $(which pyenv) ]]
+then
+  export PYENV_ROOT="$HOME/.pyenv"
+  [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init - zsh)"
 fi
