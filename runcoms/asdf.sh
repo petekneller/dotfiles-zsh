ca2b537ccd639eba041b24af8c636b2482b2df7a
Pete Kneller <pommypete@gmail.com>
2026-04-22 11:06:01 +0100

    Adds extra test to OSX ASDF install...
    for non-brew installations.

--- runcoms/asdf.sh
+++ runcoms/asdf.sh
@@ -1,4 +1,4 @@
-if [[ "$OSTYPE" == darwin* ]]; then
+if [[ "$OSTYPE" == darwin* && -a $(brew --prefix asdf) ]]; then
    # NB that despite brew adding the asdf script to PATH, it also needs to be sourced in order to add the shims to the environment
    # Somewhat odd that brew adds the script to the path instead of providing instructions to source it
    source $(brew --prefix asdf)/libexec/asdf.sh
