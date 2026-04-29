141077fb07a7bcc4562754d2f7503e8719a1bd43
Pete Kneller <pommypete@gmail.com>
2026-04-29 17:24:19 +0100

    Hacks the ASDF setup since it still needs some work

--- runcoms/asdf.sh
+++ runcoms/asdf.sh
@@ -1,5 +1,8 @@
-if [[ "$OSTYPE" == darwin* && -a $(brew --prefix asdf) ]]; then
-   # NB that despite brew adding the asdf script to PATH, it also needs to be sourced in order to add the shims to the environment
-   # Somewhat odd that brew adds the script to the path instead of providing instructions to source it
-   source $(brew --prefix asdf)/libexec/asdf.sh
-fi
+# if [[ "$OSTYPE" == darwin* && -a $(brew --prefix asdf) ]]; then
+#    # NB that despite brew adding the asdf script to PATH, it also needs to be sourced in order to add the shims to the environment
+#    # Somewhat odd that brew adds the script to the path instead of providing instructions to source it
+#    source $(brew --prefix asdf)/libexec/asdf.sh
+# fi
+
+# the above needs work - it doesn't work where asdf is not brew installed
+[[ -f $(which asdf) ]] && export PATH="${PATH}":~/.asdf/shims
