ffee59588ac4e80206ea45676331701ecfae502c
Pete Kneller <pommypete@gmail.com>
2026-09-14 16:36:03 +0100

    Adds a function to set the AWS_PROFILE based on...
    extracting profiles from AWS config and passing to fzf.

--- runcoms/aws.sh
+++ runcoms/aws.sh
@@ -1,2 +1,12 @@
 # completions, assumes the following binary was added to the PATH at installation
 [[ -f $(which aws_completer) ]] && complete -C aws_completer aws
+
+# select & set AWS_PROFILE
+if [ $(whence fzf) ]
+then
+  aws_change_profile() {
+    profiles=$(cat ~/.aws/config | grep '\[profile' | sed 's/\[profile \([a-z_\-]*\)\]/\1/')
+    selected=$(echo $profiles | fzf)
+    export AWS_PROFILE=$selected
+  }
+fi
