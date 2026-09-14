db68a593db96a920078dea583870214f833da085
Pete Kneller <pommypete@gmail.com>
2026-09-14 16:34:24 +0100

    Adds some extra status to command line prompt for asdf, terraform, etc

--- runcoms/p10k.zsh
+++ runcoms/p10k.zsh
@@ -49,10 +49,10 @@
     command_execution_time  # duration of the last command
     # background_jobs       # presence of background jobs
     direnv                # direnv status (https://direnv.net/)
-    # asdf                  # asdf version manager (https://github.com/asdf-vm/asdf)
-    # virtualenv            # python virtual environment (https://docs.python.org/3/library/venv.html)
+    asdf                  # asdf version manager (https://github.com/asdf-vm/asdf)
+    virtualenv            # python virtual environment (https://docs.python.org/3/library/venv.html)
     # anaconda              # conda environment (https://conda.io/)
-    # pyenv                 # python environment (https://github.com/pyenv/pyenv)
+    pyenv                 # python environment (https://github.com/pyenv/pyenv)
     # goenv                 # go environment (https://github.com/syndbg/goenv)
     # nodenv                # node.js version from nodenv (https://github.com/nodenv/nodenv)
     # nvm                   # node.js version from nvm (https://github.com/nvm-sh/nvm)
@@ -75,7 +75,7 @@
     # scalaenv              # scala version from scalaenv (https://github.com/scalaenv/scalaenv)
     # haskell_stack         # haskell version from stack (https://haskellstack.org/)
     kubecontext             # current kubernetes context (https://kubernetes.io/)
-    # terraform             # terraform workspace (https://www.terraform.io)
+    terraform             # terraform workspace (https://www.terraform.io)
     # terraform_version     # terraform version (https://www.terraform.io)
     aws                     # aws profile (https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html)
     # aws_eb_env            # aws elastic beanstalk environment (https://aws.amazon.com/elasticbeanstalk/)
